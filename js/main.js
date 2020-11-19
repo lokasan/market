a6f76a77fdc6a3b56f7bee6de75b086caac83556
lokasan <lokasan@inbox.ru>
2020-11-19 06:32:39 +0300

    The transfer of functions in the classes and adds classes and methods for Basket

--- js/main.js
+++ js/main.js
@@ -1,23 +1,89 @@
-const products = [
-    {id: 1, title: 'Notebook', image: 'img/orig.jpg', price: 20000},
-    {id: 2, title: 'Mouse', image: 'img/orig.jpg', price: 1500},
-    {id: 3, title: 'Keyboard', image: 'img/orig.jpg', price: 5000},
-    {id: 4, title: 'Gamepad', image: 'img/orig.jpg', price: 4500},
-];
+class ProductList {
+    #goods
+    #allProducts
+    constructor(container = '.products') {
+        this.container = container
+        this.#goods = []
+        this.#allProducts = []
+        this.#fetchGoods()
+        this.#render()
+    }
+    #fetchGoods() {
+        this.#goods = [
+            {id: 1, title: 'Notebook', image: 'img/orig.jpg', price: 20000},
+            {id: 2, title: 'Mouse', image: 'img/orig.jpg', price: 1500},
+            {id: 3, title: 'Keyboard', image: 'img/orig.jpg', price: 5000},
+            {id: 4, title: 'Gamepad', image: 'img/orig.jpg', price: 4500},
+        ]
+    }
+    #render() {
+        const container = document.querySelector(this.container)
+        for (let item of this.#goods) {
+            const prodObj = new ProductItem(item)
+            this.#allProducts.push(prodObj)
+            container.insertAdjacentHTML('beforeend', prodObj.render())
+        }
 
-const renderProduct = (title, image, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <img class='images' src=${image} alt="">
-                <p class="price">${price}</p>
+    }
+    sumOfGoods() {
+        let sum = 0
+        for (let item of this.#allProducts) {
+            sum += item.price
+        }
+        return sum
+    }
+
+}
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title
+        this.price = product.price
+        this.img = product.image
+    }
+    render() {
+        return `<div class="product-item">
+                <h3>${this.title}</h3>
+                <img class='images' src=${this.img} alt="">
+                <p class="price">${this.price}</p>
                 <button class="btn btn-success">Добавить в корзину</button>
               </div>`;
-};
+    }
+}
 
-const catalogInit = (list) => {
-    const productList = list.map((item) => renderProduct(item.title, item.image, item.price));
-    console.log(productList);
-    document.querySelector('.products').innerHTML = productList.join('');
-};
+class BasketOfGoods {
+    constructor() {
 
-catalogInit(products);
+    }
+
+    #render() {
+
+    }
+
+    clear() {
+
+    }
+
+    order() {
+
+    }
+}
+
+class BasketOfGoodsItem {
+    constructor() {
+
+    }
+    render() {
+
+    }
+    add() {
+
+    }
+    remove() {
+
+    }
+
+
+}
+const firstList = new ProductList()
+// firstList.sumOfGoods() (text in the console of browser)
